fdec731c99b34538cf0a28de9f59aeeca9e5d0d3
Mayankjha12 <mayank.jha.ug24@nsut.ac.in>
2025-10-03 23:20:23 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,9 @@
+export default {
+  plugins: {
+    // 1. Tailwind CSS processing
+    tailwindcss: {},
+    // 2. Autoprefixer for compatibility across different browsers
+    autoprefixer: {},
+  },
+}
 
